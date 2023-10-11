431785c3ebe9fc36ca40fc305b5ec6efb07917a6
tinykite <dakota@tinykitelab.com>
2023-10-11 12:09:42 -0500

    feat: Cleanup how css reset is handled for web components

--- components/caselawmap.js
+++ components/caselawmap.js
@@ -1,25 +1,35 @@
 import {LitElement, css, html} from '../lib/lit.js';
+import { baseStyles } from '../lib/wc-base.js';
 
-export class CaselawMap extends LitElement {
+export class CaselawMap extends LitElement  {
   static properties = {
-    name: {},
   };
-  // Define scoped styles right with your component, in plain CSS
-  static styles = css`
-    :host {
-      color: blue;
-    }
-  `;
+
+  static styles = [
+    baseStyles,
+    css`
+      .background {
+        background: var(--gradient);
+      }
+    `
+  ];
 
   constructor() {
     super();
-    // Declare reactive properties
-    this.name = 'World';
   }
 
-  // Render the UI as a function of component state
   render() {
-    return html`<p>Hello, ${this.name}!</p>`;
+    return html`
+    <div class="background">		
+    <h2>Our data</h2>
+		<p>360 years of United States caselaw</p>
+
+		<h3>State and Federal Totals</h3>
+		<p>6,930,777<br /><span>Unique cases</span><p>
+
+		<h3>Federal Totals</h3>
+    </div>
+    `;
   }
 }
 customElements.define('caselaw-map', CaselawMap);
