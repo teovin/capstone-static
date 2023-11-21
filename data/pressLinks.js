41fa3e46ffa0248bb7aae10b0378ef158de7d181
tinykite <dakota@tinykitelab.com>
2023-11-21 13:31:19 -0600

    feat: Add section highlight component

---
+++ data/pressLinks.js
@@ -0,0 +1,34 @@
+export const pressLinks = [
+	{
+		title: "A Closer Look at the Caselaw Access Project",
+		publisher: "AALL Spectrum",
+		date: "May / June 2020",
+		url: "https://aallspectrum.aallnet.org/html5/reader/production/default.aspx?pnum=23&edid=d98eccf1-bdd8-4cb6-8c06-1fd8ed81bec0",
+	},
+	{
+		title: `New ‘Historical Trends’ Viewer from Caselaw Access Project Graphs Frequency of Words and Phrases`,
+		publisher: "LawSites",
+		date: "June 21, 2019",
+		url: "https://www.lawnext.com/2019/06/new-historical-trends-viewer-from-caselaw-access-project-graphs-frequency-of-words-and-phrases.html",
+	},
+	{
+		title:
+			"New tool by Harvard Law lets people explore language usage in caselaw",
+		publisher: "ABA Journal",
+		date: "June 20, 2019",
+		url: "https://www.abajournal.com/news/article/new-tool-lets-people-explore-language-usage-in-caselaw",
+	},
+	{
+		title:
+			"LawNext Episode 18: Adam Ziegler on How Harvard Put 360 Years of Caselaw Online",
+		publisher: "LawSites",
+		date: "November 12, 2018",
+		url: "https://www.lawsitesblog.com/2018/11/lawnext-episode-18-adam-ziegler-harvard-put-360-years-caselaw-online.html",
+	},
+	{
+		title: "Harvard Opens Up Its Massive Caselaw Access Project",
+		publisher: "TechDirt",
+		date: "October 31, 2018",
+		url: "https://www.techdirt.com/2018/10/31/harvard-opens-up-massive-caselaw-access-project/",
+	},
+];
