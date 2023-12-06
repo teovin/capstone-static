3f6d058ec58f0daebc483198e62e2b2eca927357
tinykite <dakota@tinykitelab.com>
2023-12-06 10:42:59 -0600

    feat: Add contributors list

---
+++ data/contributors.js
@@ -0,0 +1,60 @@
+export const contributors = [
+	{
+		name: "Anastasia Aizman",
+		job_title: "Designer and Lead Creative Technologist",
+	},
+	{ name: "Kendra Albert", job_title: "Research Associate" },
+	{
+		name: "Karen Beck",
+		job_title: "Manager, Historical & Special Collections",
+	},
+	{ name: "Zachary Bodnar", job_title: "Digitization Specialist" },
+	{
+		name: "June Casey",
+		job_title:
+			"Librarian for Open Access Initiatives & Scholarly Communication",
+	},
+	{
+		name: "Stephen Chapman",
+		job_title: "Manager, Digital Strategies for Collections",
+	},
+	{ name: "Deborah Chase", job_title: "Digitization Specialist" },
+	{ name: "Jack Cushman", job_title: "Director" },
+	{ name: "Kim Dulin", job_title: "Library Innovation Lab Director" },
+	{ name: "Lindsay Dumas", job_title: "Digital Projects Archivist" },
+	{ name: "Kate Edrington", job_title: "Digitization Specialist" },
+	{ name: "Harmony Eidolon", job_title: "Program Coordinator" },
+	{ name: "Kelly Fitzpatrick", job_title: "Research Associate" },
+	{ name: "Kerri Fleming", job_title: "Digital Projects Archivist" },
+	{ name: "Gerard Fowke", job_title: "2018 Harvard Law School Library Intern" },
+	{ name: "Andy Gu", job_title: "2021 Summer Research Assistant" },
+	{
+		name: "Jane Kelly",
+		job_title: "Historical & Special Collections Assistant",
+	},
+	{ name: "Erica Leeman", job_title: "Digitization Specialist" },
+	{ name: "Dustin Lewis", job_title: "Project Manager" },
+	{ name: "Andrew MacTaggart", job_title: "Senior Digitization Specialist" },
+	{ name: "Emily Magagnosc", job_title: "Digitization Specialist" },
+	{ name: "Margaret Peachy", job_title: "Curator of Digital Collections" },
+	{ name: "Lori Schulsinger", job_title: "Collection Development Coordinator" },
+	{ name: "Andy Silva", job_title: "Developer" },
+	{ name: "Ben Steinberg", job_title: "DevOps" },
+	{
+		name: "Shailin Thomas",
+		job_title: "Affiliate, Berkman Center for Internet & Society",
+	},
+	{
+		name: "Caroline Walters",
+		job_title: "Collection Development Librarian for U.S. Law",
+	},
+	{
+		name: "Suzanne Wones",
+		job_title: "Executive Director, Harvard Law School Library",
+	},
+	{ name: "Adam Ziegler", job_title: "Director" },
+	{
+		name: "Jonathan Zittrain",
+		job_title: "Harvard Faculty and Law School Library Director",
+	},
+];
