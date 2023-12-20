995d33b200eadb86f5b67cb483cf999ab2842aaa
tinykite <dakota@tinykitelab.com>
2023-12-20 11:59:35 -0600

    feat: Update contributor list

--- data/contributors.js
+++ data/contributors.js
@@ -1,7 +1,23 @@
-export const contributors = [
+export const contributorLinks = [
+	{
+		name: "Dakota Sexton",
+		job_title: "Senior Software Engineer",
+		url: "https://www.tinykitelab.com",
+	},
+	{
+		name: "Rebecca Kilberg",
+		job_title: "Technologist in Residence",
+		url: "https://lil.law.harvard.edu/about/#rebecca-kilberg",
+	},
+	{
+		name: "Clare Stanton",
+		job_title: "Product and Research Manager",
+		url: "https://lil.law.harvard.edu/about/#clare-stanton",
+	},
 	{
 		name: "Anastasia Aizman",
 		job_title: "Designer and Lead Creative Technologist",
+		url: "https://lil.law.harvard.edu/about/#anastasia-aizman",
 	},
 	{ name: "Kendra Albert", job_title: "Research Associate" },
 	{
@@ -17,32 +33,74 @@ export const contributors = [
 	{
 		name: "Stephen Chapman",
 		job_title: "Manager, Digital Strategies for Collections",
+		url: "https://lil.law.harvard.edu/about/#stephen-chapman",
 	},
 	{ name: "Deborah Chase", job_title: "Digitization Specialist" },
-	{ name: "Jack Cushman", job_title: "Director" },
-	{ name: "Kim Dulin", job_title: "Library Innovation Lab Director" },
+	{
+		name: "Jack Cushman",
+		job_title: "Director",
+		url: "https://lil.law.harvard.edu/about/#jack-cushman",
+	},
+	{
+		name: "Kim Dulin",
+		job_title: "Library Innovation Lab Director",
+		url: "https://lil.law.harvard.edu/about/#kim-dulin",
+	},
 	{ name: "Lindsay Dumas", job_title: "Digital Projects Archivist" },
 	{ name: "Kate Edrington", job_title: "Digitization Specialist" },
-	{ name: "Harmony Eidolon", job_title: "Program Coordinator" },
-	{ name: "Kelly Fitzpatrick", job_title: "Research Associate" },
-	{ name: "Kerri Fleming", job_title: "Digital Projects Archivist" },
+	{
+		name: "Harmony Eidolon",
+		job_title: "Program Coordinator",
+		url: "https://lil.law.harvard.edu/about/#harmony-eidolon",
+	},
+	{
+		name: "Kelly Fitzpatrick",
+		job_title: "Research Associate",
+		url: "https://lil.law.harvard.edu/about/#kelly-fitzpatrick",
+	},
+	{
+		name: "Kerri Fleming",
+		job_title: "Digital Projects Archivist",
+		url: "https://lil.law.harvard.edu/about/#kerri-fleming",
+	},
 	{ name: "Gerard Fowke", job_title: "2018 Harvard Law School Library Intern" },
-	{ name: "Andy Gu", job_title: "2021 Summer Research Assistant" },
+	{
+		name: "Andy Gu",
+		job_title: "2021 Summer Research Assistant",
+		url: "https://lil.law.harvard.edu/about/#andy-gu",
+	},
 	{
 		name: "Jane Kelly",
 		job_title: "Historical & Special Collections Assistant",
 	},
 	{ name: "Erica Leeman", job_title: "Digitization Specialist" },
-	{ name: "Dustin Lewis", job_title: "Project Manager" },
-	{ name: "Andrew MacTaggart", job_title: "Senior Digitization Specialist" },
+	{
+		name: "Dustin Lewis",
+		job_title: "Project Manager",
+		url: "https://lil.law.harvard.edu/about/#dustin-lewis",
+	},
+	{
+		name: "Andrew MacTaggart",
+		job_title: "Senior Digitization Specialist",
+		url: "https://lil.law.harvard.edu/about/#andrew-mactaggart",
+	},
 	{ name: "Emily Magagnosc", job_title: "Digitization Specialist" },
 	{ name: "Margaret Peachy", job_title: "Curator of Digital Collections" },
 	{ name: "Lori Schulsinger", job_title: "Collection Development Coordinator" },
-	{ name: "Andy Silva", job_title: "Developer" },
-	{ name: "Ben Steinberg", job_title: "DevOps" },
+	{
+		name: "Andy Silva",
+		job_title: "Developer",
+		url: "https://lil.law.harvard.edu/about/#andy-silva",
+	},
+	{
+		name: "Ben Steinberg",
+		job_title: "DevOps",
+		url: "https://arc.net/l/quote/mqvsiuka",
+	},
 	{
 		name: "Shailin Thomas",
 		job_title: "Affiliate, Berkman Center for Internet & Society",
+		url: "https://lil.law.harvard.edu/about/#shailin-thomas",
 	},
 	{
 		name: "Caroline Walters",
@@ -51,10 +109,16 @@ export const contributors = [
 	{
 		name: "Suzanne Wones",
 		job_title: "Executive Director, Harvard Law School Library",
+		url: "https://lil.law.harvard.edu/about/#suzanne-wones",
 	},
-	{ name: "Adam Ziegler", job_title: "Director" },
+	{
+		name: "Adam Ziegler",
+		job_title: "Director",
+		url: "https://lil.law.harvard.edu/about/#adam-ziegler",
+	},
 	{
 		name: "Jonathan Zittrain",
 		job_title: "Harvard Faculty and Law School Library Director",
+		url: "https://lil.law.harvard.edu/about/#jonathan-zittrain",
 	},
 ];
