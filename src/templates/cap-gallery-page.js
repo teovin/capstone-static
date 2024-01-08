9190255e5d7281c6908f0a7e923e7c7221bf36e0
tinykite <dakota@tinykitelab.com>
2024-01-08 08:43:55 -0600

    feat: Add gallery page template
    (cherry picked from commit 0d9f26070da8a82961f8ae9c9e57c9e9b2e2093b)

---
+++ src/templates/cap-gallery-page.js
@@ -0,0 +1,48 @@
+import { LitElement, html } from "../lib/lit.js";
+import "../components/cap-nav.js";
+import "../components/cap-page-header.js";
+import "../components/cap-footer.js";
+import "../components/cap-anchor-list.js";
+import "../components/cap-media-list.js";
+import "../components/cap-contributor-list.js";
+import "../components/cap-gallery-item.js";
+
+import { anchorLinks } from "../data/aboutSidebarLinks.js";
+
+export class CapGalleryPage extends LitElement {
+	// Turn Shadow DOM off
+	// Generally discouraged: https://lit.dev/docs/components/shadow-dom/#implementing-createrenderroot
+	createRenderRoot() {
+		return this;
+	}
+
+	render() {
+		return html`
+			<cap-nav></cap-nav>
+			<main id="main" class="l-interiorPage">
+				<header class="u-bg-gray-500 u-col-span-full">
+					<cap-page-header heading="About">
+						<p class="u-text-white u-text-serif">
+							The Caselaw Access Project (“CAP”) expands public access to U.S.
+							law. Our goal is to make all published U.S. court decisions freely
+							available to the public online, in a consistent format, digitized
+							from the collection of the Harvard Law School Library.
+						</p>
+					</cap-page-header>
+				</header>
+				<aside class="u-w-fit u-sm-hidden">
+					<cap-anchor-list .data=${anchorLinks}></cap-anchor-list>
+				</aside>
+				<article class="c-article u-bg-beige">
+					<cap-gallery-item
+						title="Modeling the Caselaw Access Project"
+						description="Article by Felix B. Chang, Erin McCabe, and James Lee, 22 Nevada Law Journal (2022)"
+						link="/test.html"
+					></cap-gallery-item>
+				</article>
+			</main>
+			<cap-footer></cap-footer>
+		`;
+	}
+}
+customElements.define("cap-gallery-page", CapGalleryPage);
