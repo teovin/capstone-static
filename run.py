376ff5e8da0291350b5684656a5e016e7979f13e
Rebecca Cremona <rcremona@law.harvard.edu>
2024-01-25 13:43:26 -0500

    Serve our 404 locally, via the python server.

--- run.py
+++ run.py
@@ -3,12 +3,19 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler, test
 
 
 class CORSRequestHandler (SimpleHTTPRequestHandler):
+
     def end_headers (self):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods', '*')
         self.send_header('Access-Control-Allow-Headers', '*')
         self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
         SimpleHTTPRequestHandler.end_headers(self)
+
+    def send_error(self, code, message=None):
+        if code == 404:
+            with open('404.html') as not_found:
+                self.error_message_format = not_found.read()
+        SimpleHTTPRequestHandler.send_error(self, code, message)
 
 
 if __name__ == '__main__':
