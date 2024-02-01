22de42ea34b8838dad954b4ccbdee88bae44294b
R I B Z <kilbergr@users.noreply.github.com>
2024-02-01 13:25:33 -0800

    Adds some content to accessing data section

--- src/templates/cap-docs-page.js
+++ src/templates/cap-docs-page.js
@@ -31,7 +31,25 @@ export class CapDocsPage extends LitElement {
 				<article class="c-article u-bg-beige">
 					<h2 class="c-decoratedHeader" id="accessing-data">Accessing Data</h2>
 					<p>
-						[We will put some documentation content about accessing data here.]
+						The CAP website offers a basic interface for interacting with the
+						cases in our repository. To access reporters by jurisdiction, visit
+						our <a href="/caselaw">Caselaw</a> page and select the
+						jurisdiction of interest. The reporters included in that
+						jurisdiction will be listed there.
+					</p>
+					<p>
+						To access volumes by reporter, select the reporter from the Caselaw
+						page to view the volumes within that reporter.
+					</p>
+					<p>
+						To access individual cases, first select jurisdiction, then
+						reporter, and finally the volume of interest. Once on the individual
+						volume page, you will be able to access all of the cases in that
+						volume.
+					</p>
+					<p>
+						See our <a href="/about">About</a> page for more details on the
+						materials included.
 					</p>
 					<h2 class="c-decoratedHeader" id="bulk-downloads">Bulk Downloads</h2>
 					<p>
